c2088fe6f5ba1f14d90d4b7671e71c0aac53fee1
Himanshu <officialhimanshutyagi@gmail.com>
2026-09-16 11:21:17 +0530

    Clean up temporary python script files

--- check_header.py
+++
@@ -1,6 +0,0 @@
-import os
-with open('ai-impact-on-healthcare.html', 'r', encoding='utf-8') as f:
-    text = f.read()
-    start = text.find('<header class="blog-header">')
-    end = text.find('</header>')
-    print(text[start:end+10])
